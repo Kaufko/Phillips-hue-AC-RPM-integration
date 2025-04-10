e225a39132133caca06c50300f06cb3fa1f2053a
Kaufko <99084056+Kaufko@users.noreply.github.com>
2025-04-10 16:57:26 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,13 +8,10 @@ namespace HueApi.Entertainment.ConsoleSample
     {
         public static async Task Main(string[] args)
         {
-            Console.WriteLine("HueApi Entertainment V2 Sample App");
             Console.WriteLine("Edit your bridge keys in StreamingSetup.cs");
 
             HueStreaming s = new HueStreaming();
             await s.Start();
-
-            Console.WriteLine("finished");
 
             Console.ReadLine();
 
